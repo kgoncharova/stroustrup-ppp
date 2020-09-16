222c8ab8bf97c7ef39a3eaedc9012700dc0f96f3
Kristina <codingkris@yahoo.com>
2020-09-16 13:51:30 +0300

    Completed exercise 04

--- chapter03/ex03_bad_names.cpp
+++ chapter03/ex03_bad_names.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-  int double = 5.52; // error: expected unqualified-id before ‘=’ token
-  string 1;          // error: expected unqualified-id before numeric constant
-  int string = 10;   // warning: unused variable ‘string’ [-Wunused-variable]
+    int double = 5.52; // error: expected unqualified-id before ‘=’ token
+    string 1;          // error: expected unqualified-id before numeric constant
+    int string = 10;   // warning: unused variable ‘string’ [-Wunused-variable]
 }
---
+++ chapter03/ex04_two_integers.cpp
@@ -0,0 +1,22 @@
+#include "../lib_files/std_lib_facilities.h"
+
+int main()
+{
+    int val1;
+    int val2;
+
+    cout << "Enter two integers\n";
+    cin >> val1 >> val2;
+
+    if (val1 < val2)
+        cout << val1 << " is the lesser\n"
+             << val2 << " is the greater\n";
+    if (val1 > val2)
+        cout << val2 << " is the lesser\n"
+             << val1 << " is the greater\n";
+
+    cout << "Their sum is " << val1 + val2 << '\n'
+         << "Their difference is " << val1 - val2 << '\n'
+         << "Their product is " << val1 * val2 << '\n'
+         << "Their ratio is " << val1 / val2 << '\n';
+}
