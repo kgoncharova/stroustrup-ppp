08efea3506c87666baa2ddae9118e9e750cc1e6e
Kristina <codingkris@yahoo.com>
2020-09-16 13:14:27 +0300

    Completed exercise 03

---
+++ chapter03/ex03_bad_names.cpp
@@ -0,0 +1,8 @@
+#include "../lib_files/std_lib_facilities.h"
+
+int main()
+{
+	int double = 5.52; // error: expected unqualified-id before ‘=’ token
+	string 1; // error: expected unqualified-id before numeric constant
+	int string = 10; // warning: unused variable ‘string’ [-Wunused-variable]
+}
