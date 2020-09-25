e27ea7d27bdc8e0aec2147f3c9c1a27de8e220a3
Kristina <codingkris@yahoo.com>
2020-09-25 11:19:43 +0300

    Completed exercise 02

---
+++ chapter04/ex02.cpp
@@ -0,0 +1,26 @@
+#include "../lib_files/std_lib_facilities.h"
+
+int main()
+{
+    vector<double> temps;
+    double median = 0;
+    double sum = 0;
+
+    for (double temp; cin>>temp; )
+        temps.push_back(temp);
+
+    int above_mid = temps.size()/2;
+
+    for (double x : temps) sum += x;
+    cout << "Average temperature: " << sum/temps.size() << '\n';
+    sort(temps);
+
+    if (temps.size() % 2 == 0) {
+        median = (temps[above_mid] + temps[above_mid - 1])/2;
+    } else {
+        median = temps[above_mid];
+    }
+    cout << "Median temperature: " << median << endl;
+
+    return 0;
+}
