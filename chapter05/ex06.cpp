65bedf85c8e35d39464bde6bc2b4f6c101929178
Kristina <codingkris@yahoo.com>
2020-10-19 08:08:03 +0300

    Completed exercise 06

---
+++ chapter05/ex06.cpp
@@ -0,0 +1,43 @@
+#include "../lib_files/std_lib_facilities.h"
+
+double ctof(double c)
+{
+    double f = 1.8*c + 32;
+    return f;
+}
+
+double ftoc(double f)
+{
+    double c = (f-32)/1.8;
+    return c;
+}
+
+int main()
+{
+    double c = 0.0;
+    double f = 0.0;
+    char ch;
+
+    cout << "Enter a temperature in Celsius or Fahrenheit <C/F>: ";
+    cin >> ch;
+    
+    switch(ch)
+    {
+        case 'F':
+        case 'f':
+            cin >> f;
+            c = ftoc(f);
+            cout << f << "F" << " converts to " << c << "C" << '\n';
+            break;
+        case 'C':
+        case 'c':
+            cin >> c;
+            f = ctof(c);
+            cout << c << "C" << " converts to " << f << "F" << '\n';
+            break;
+        default:
+            cout << "Bye!";
+    }
+
+    return 0;
+}
