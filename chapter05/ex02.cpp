eb8a93f65273fdfca6653f500c6cb994404586d3
Kristina <codingkris@yahoo.com>
2020-10-19 07:32:46 +0300

    Completed exercise 02

---
+++ chapter05/ex02.cpp
@@ -0,0 +1,17 @@
+#include "../lib_files/std_lib_facilities.h"
+
+double ctok(double c)
+{
+    double k = c + 273.15;
+    return k;
+}
+
+int main()
+{
+    double c = 0;
+    cin >> c;
+    double k = ctok(c);
+    cout << k << '\n';
+
+    return 0;
+}
