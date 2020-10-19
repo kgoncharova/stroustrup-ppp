c1382bc0611a7b1add573afda0a66b7601c57b60
Kristina <codingkris@yahoo.com>
2020-10-19 07:38:28 +0300

    Completed exercise 04

---
+++ chapter05/ex04.cpp
@@ -0,0 +1,20 @@
+#include "../lib_files/std_lib_facilities.h"
+
+double ctok(double c)
+{
+    if (c < -273.15) error("Input temp is lower than absolute 0.\n");
+
+    double k = c + 273.15;
+    return k;
+}
+
+int main()
+{
+    double c = 0;
+    cin >> c;
+
+    double k = ctok(c);
+    cout << k << '\n';
+
+    return 0;
+}
